3e3117f2ec75cd73d94c28299909a8c538102c43
denisomar <denisomar@hotmail.com.ar>
2021-11-16 23:32:27 -0300

    commit entidad tipoTramite

---
+++ src/main/java/com/gestion/tramite/entidad/TipoTramite.java
@@ -0,0 +1,25 @@
+package com.gestion.tramite.entidad;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tipoTramite")
+@Data
+public class TipoTramite
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String tipo;
+    private String descripcion;
+    private double presupuesto;
+    private Date fechaAlta;
+    private Integer estado;
+
+
+
+}
